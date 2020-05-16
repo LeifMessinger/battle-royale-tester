52380e249d48599cb21cd761e5a7a2f5f153c792
Leif Messinger <leifmessinger@gmail.com>
2020-05-16 03:26:45 -0500

    idk man

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const http = require('http');
 const port = process.env.PORT || process.argv[2] || 8080;
 const simple = require('noframe-http');
 const server = http.createServer(simple.sauce);
+const battleroyale = require('battle-royale');
+const rps = require('rps-br');
 server.listen(port);
 console.log('Started at ' + port);
